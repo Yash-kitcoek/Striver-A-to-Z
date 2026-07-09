91517518594b5d6f8eaeb96ffbf561627c3ca2a9
Yash <yashgaikwad3295@gmail.com>
2026-07-10 02:01:23 +0530

    remove duplicate from sorted array

--- Array/04_remove_duplicate.cpp
+++ Array/04_remove_duplicate.cpp
@@ -3,23 +3,40 @@
 #include<unordered_set>
 using namespace std;
 
+
+// Brute Force O(n), O(n)
+
+// int remove_duplicates(vector<int> &nums) {
+//     unordered_set<int> seen;
+
+//     int index = 0;
+
+//     for(int num : nums) {
+//         if(seen.find(num) == seen.end()) {
+//             seen.insert(num);
+
+//             nums[index] = num;
+
+//             index++;
+//         }
+//     }
+//     return index;
+// }
+
+// Optimal O(n), O(1)
+
 int remove_duplicates(vector<int> &nums) {
-    unordered_set<int> seen;
+    int i = 0;
 
-    int index = 0;
+    for(int j=1; j<nums.size(); j++) {
+        if(nums[j] != nums[i]) {
+            i++;
 
-    for(int num : nums) {
-        if(seen.find(num) == seen.end()) {
-            seen.insert(num);
-
-            nums[index] = num;
-
-            index++;
+            nums[i] = nums[j];
         }
     }
-    return index;
+    return i+1;
 }
-
 
 int main() {
     vector<int> nums = {0,0,1,1,4,4,5,5};
