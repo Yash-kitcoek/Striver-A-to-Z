c083353a64673fb66ea4bf8ad08d2ad21ce25249
Yash <yashgaikwad3295@gmail.com>
2026-07-16 23:06:17 +0530

    array_contain_negative

--- Array/13_Longest_Subarray_with_Sum_K2.cpp
+++ Array/13_Longest_Subarray_with_Sum_K2.cpp
@@ -3,7 +3,7 @@
 #include<unordered_map>
 using namespace std;
 
-int longest_subarray_sum(vector<int> &nums, int k ) {
+int longest_subarray_sum(vector<int> &nums) {
     unordered_map<long long, int> mp;
     int sum = 0;
     int maxlen = 0;
@@ -11,12 +11,12 @@ int longest_subarray_sum(vector<int> &nums, int k ) {
     for(int i=0; i<nums.size(); i++) {
         sum += nums[i];
 
-        if(sum == k) {
+        if(sum == 0) {
             maxlen =  i + 1;
         }
 
-        if(mp.find(sum-k) != mp.end()) {
-            maxlen = max(maxlen, i-mp[sum - k]);
+        if(mp.find(sum) != mp.end()) {
+            maxlen = max(maxlen, i-mp[sum]);
         }
 
         if(mp.find(sum) == mp.end()) {
@@ -29,9 +29,7 @@ int longest_subarray_sum(vector<int> &nums, int k ) {
 int main() {
     vector<int> nums = {10, 5, 2, 7, 1, -10};
 
-    int k = 15;
-
-   cout <<   longest_subarray_sum(nums, k);
+   cout <<   longest_subarray_sum(nums);
 
    return 0;
 }
