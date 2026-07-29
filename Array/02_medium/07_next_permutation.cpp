611389901710896a4c2467620fb2837872a62c75
Yash <yashgaikwad3295@gmail.com>
2026-07-30 01:50:59 +0530

    next_permutation_31

---
+++ Array/02_medium/07_next_permutation.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void nextPermutation(vector<int> &nums) {
+    int n = nums.size();
+
+    int ind = -1;
+    
+    for(int i=n-2; i>=0; i--) {
+        if(nums[i] < nums[i+1]) {
+            ind = i;
+            break;
+        }
+    }
+
+    if(ind == -1) {
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+
+    for(int i=n-1; i > ind; i--) {
+        if(nums[i] > nums[ind]) {
+            swap(nums[i], nums[ind]);
+            break;
+        }
+    }
+    
+    reverse(nums.begin()+ind+1, nums.end());
+}
+
+int main() {
+    vector<int> nums = {1,2,3,6,5,4};
+
+    nextPermutation(nums);
+
+    for(int x : nums) {
+        cout << x << " ";
+    }
+}
