027610c1795ece4400c56574bd8f5f539ba42b69
Yash <yashgaikwad3295@gmail.com>
2026-08-02 18:49:49 +0530

    longest_consecuive_subsequence_leetcode128

---
+++ Array/02_medium/10_longest-consecutive-subsequence.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+
+int longestConseSeq(vector<int> &nums) {
+    unordered_set<int> st(nums.begin(), nums.end());
+    
+    int longest = 0;
+    
+
+    for(int num : nums) {
+        if(st.find(num-1) == st.end()) {
+            int curr = num;
+            int count = 1;
+
+            while(st.find(curr+1) != st.end()) {
+                curr++;
+                count++;
+            }
+
+            longest = max(longest, count);
+        }
+    }
+
+
+    return longest;
+}
+
+int main() {
+    vector<int> nums = {2 , 1, 9, 4, 5, 3};
+
+    cout << longestConseSeq(nums);
+
+}
