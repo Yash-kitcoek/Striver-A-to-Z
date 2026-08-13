b904e45995722039d0190155791346ee98b741bd
Yash <yashgaikwad3295@gmail.com>
2026-08-14 00:37:00 +0530

    search_in_rotated_arr

---
+++ 02_Binary_search/01_BS_on_1D_array/06_search_in_rotated_sorted_arr.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int search(vector<int> &nums, int target) {
+    int n = nums.size();
+
+    int low = 0;
+    int high = n-1;
+
+    while(low <= high) {
+        int mid = (low + high)/2;
+
+        if(nums[mid] == target) {
+            return mid;
+        }
+           
+
+        if(nums[low] <= nums[mid]) {
+            if(nums[low] <= target && target < nums[mid]) {
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+        }else {
+            if(target > nums[mid] && target <= nums[high]) {
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+
+    cout << search(nums, target);
+}
