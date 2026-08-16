a3edc78b0be76109eadb4bc7466288ba1fcc0853
Yash <yashgaikwad3295@gmail.com>
2026-08-17 00:16:26 +0530

    sqrt of number

---
+++ 02_Binary_search/02_BS_on_Answers/01_sqrt_of_num.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int sqrt(int x) {
+    int low = 0;
+    int high = x;
+
+    int ans = 0;
+
+    while(low <= high) {
+        int mid = low + (high - low)/2;
+
+        if(mid * mid == x) {
+            return mid;
+        }else if(mid * mid < x) {
+            ans = mid;
+            low = mid + 1;
+        }else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int x = 4;
+
+    cout << sqrt(x);
+}
