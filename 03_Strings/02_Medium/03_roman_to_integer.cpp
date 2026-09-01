df29bb7b78902b857b22ba17c2654513df4f0b09
Yash <yashgaikwad3295@gmail.com>
2026-09-01 19:07:11 +0530

    roman to Interger

---
+++ 03_Strings/02_Medium/03_roman_to_integer.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+using namespace std;
+
+int rom_to_num(string s) {
+    unordered_map<char, int> roman {
+        {'I',1},
+        {'V',5},
+        {'X',10},
+        {'L',50},
+        {'C',100},
+        {'D',500},
+        {'M',1000}
+    };
+
+    int res = 0;
+
+    for(int i = 0; i < s.size()-1; i++) {
+        if(roman[s[i]] < roman[s[i+1]]) {
+            res -= roman[s[i]];
+        }else {
+            res += roman[s[i]];
+        }
+    }
+    return res + roman[s.back()];
+}
+
+int main() {
+    string s = "MCMXCIV";
+
+    cout << rom_to_num(s);
+}
